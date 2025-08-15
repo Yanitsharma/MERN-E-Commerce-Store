6e7b25a4cb9e1b6cfa456b79829acb312da43997
Yanitsharma <yanit.sharma.mec22@itbhu.ac.in>
2025-08-16 00:16:23 +0530

    first commit

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 const { ObjectId } = mongoose.Schema;
-
 const reviewSchema = mongoose.Schema(
   {
     name: { type: String, required: true },
@@ -21,7 +20,7 @@ const productSchema = mongoose.Schema(
     image: { type: String, required: true },
     brand: { type: String, required: true },
     quantity: { type: Number, required: true },
-    category: { type: ObjectId, ref: "Category", required: true },
+    category: { type: String, ref: "Category", required: true },
     description: { type: String, required: true },
     reviews: [reviewSchema],
     rating: { type: Number, required: true, default: 0 },
